d9e08480a00c2f41754455eebc6e9d0841915275
elssbbboy <elssbbboy@gmail.com>
2018-09-27 13:46:10 +0200

    Project uploaded

---
+++ PowerManagement/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿using System.Windows.Forms;
+
+namespace PowerManagement
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.comboBoxPower = new System.Windows.Forms.ComboBox();
+            this.ApplyButton = new System.Windows.Forms.Button();
+            this.QuitButton = new System.Windows.Forms.Button();
+            this.CurrentPlan = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.powerManagementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuNotifyIcon = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripSeparator();
+            this.quitToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripSeparator();
+            this.quitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.Hide = new System.Windows.Forms.Button();
+            this.startSystemTrayCheckbox = new System.Windows.Forms.CheckBox();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuNotifyIcon.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBoxPower
+            // 
+            this.comboBoxPower.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxPower.FormattingEnabled = true;
+            this.comboBoxPower.Location = new System.Drawing.Point(40, 62);
+            this.comboBoxPower.Name = "comboBoxPower";
+            this.comboBoxPower.Size = new System.Drawing.Size(452, 21);
+            this.comboBoxPower.TabIndex = 0;
+            this.comboBoxPower.SelectedIndexChanged += new System.EventHandler(this.comboBoxPower_SelectedIndexChanged);
+            // 
+            // ApplyButton
+            // 
+            this.ApplyButton.Location = new System.Drawing.Point(539, 37);
+            this.ApplyButton.Name = "ApplyButton";
+            this.ApplyButton.Size = new System.Drawing.Size(75, 40);
+            this.ApplyButton.TabIndex = 1;
+            this.ApplyButton.Text = "Apply";
+            this.ApplyButton.UseVisualStyleBackColor = true;
+            this.ApplyButton.Click += new System.EventHandler(this.ApplyButton_Click);
+            // 
+            // QuitButton
+            // 
+            this.QuitButton.Location = new System.Drawing.Point(539, 127);
+            this.QuitButton.Name = "QuitButton";
+            this.QuitButton.Size = new System.Drawing.Size(75, 40);
+            this.QuitButton.TabIndex = 2;
+            this.QuitButton.Text = "Quit";
+            this.QuitButton.UseVisualStyleBackColor = true;
+            this.QuitButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // CurrentPlan
+            // 
+            this.CurrentPlan.AutoSize = true;
+            this.CurrentPlan.Location = new System.Drawing.Point(37, 37);
+            this.CurrentPlan.Name = "CurrentPlan";
+            this.CurrentPlan.Size = new System.Drawing.Size(68, 13);
+            this.CurrentPlan.TabIndex = 3;
+            this.CurrentPlan.Text = "Current Plan:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(506, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Created by elssbbboy";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.powerManagementToolStripMenuItem,
+            this.ayudaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(634, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // powerManagementToolStripMenuItem
+            // 
+            this.powerManagementToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirToolStripMenuItem});
+            this.powerManagementToolStripMenuItem.Name = "powerManagementToolStripMenuItem";
+            this.powerManagementToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.powerManagementToolStripMenuItem.Text = "File";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(97, 22);
+            this.salirToolStripMenuItem.Text = "Quit";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.ayudaToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuNotifyIcon;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "PowerManagement";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuNotifyIcon
+            // 
+            this.contextMenuNotifyIcon.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.quitToolStripMenuItem,
+            this.quitToolStripMenuItem2});
+            this.contextMenuNotifyIcon.Name = "contextMenuNotifyIcon";
+            this.contextMenuNotifyIcon.Size = new System.Drawing.Size(104, 60);
+            this.contextMenuNotifyIcon.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.contextMenuNotifyIcon_ItemClicked);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(100, 6);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(100, 6);
+            // 
+            // quitToolStripMenuItem2
+            // 
+            this.quitToolStripMenuItem2.Name = "quitToolStripMenuItem2";
+            this.quitToolStripMenuItem2.Size = new System.Drawing.Size(103, 22);
+            this.quitToolStripMenuItem2.Text = "Quit";
+            this.quitToolStripMenuItem2.Click += new System.EventHandler(this.quitToolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(149, 6);
+            // 
+            // quitToolStripMenuItem1
+            // 
+            this.quitToolStripMenuItem1.Name = "quitToolStripMenuItem1";
+            this.quitToolStripMenuItem1.Size = new System.Drawing.Size(32, 19);
+            // 
+            // Hide
+            // 
+            this.Hide.Location = new System.Drawing.Point(539, 82);
+            this.Hide.Name = "Hide";
+            this.Hide.Size = new System.Drawing.Size(75, 40);
+            this.Hide.TabIndex = 7;
+            this.Hide.Text = "Hide to System Tray";
+            this.Hide.UseVisualStyleBackColor = true;
+            this.Hide.Click += new System.EventHandler(this.Hide_Click);
+            // 
+            // startSystemTrayCheckbox
+            // 
+            this.startSystemTrayCheckbox.AutoSize = true;
+            this.startSystemTrayCheckbox.Location = new System.Drawing.Point(40, 176);
+            this.startSystemTrayCheckbox.Name = "startSystemTrayCheckbox";
+            this.startSystemTrayCheckbox.Size = new System.Drawing.Size(120, 17);
+            this.startSystemTrayCheckbox.TabIndex = 8;
+            this.startSystemTrayCheckbox.Text = "Start in System Tray";
+            this.startSystemTrayCheckbox.UseVisualStyleBackColor = true;
+            this.startSystemTrayCheckbox.CheckedChanged += new System.EventHandler(this.startSystemTrayCheckbox_CheckedChanged);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 211);
+            this.Controls.Add(this.startSystemTrayCheckbox);
+            this.Controls.Add(this.Hide);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CurrentPlan);
+            this.Controls.Add(this.QuitButton);
+            this.Controls.Add(this.ApplyButton);
+            this.Controls.Add(this.comboBoxPower);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.Text = "PowerManagement";
+            this.Shown += new System.EventHandler(this.formShown_);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuNotifyIcon.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxPower;
+        private System.Windows.Forms.Button ApplyButton;
+        private System.Windows.Forms.Button QuitButton;
+        private System.Windows.Forms.Label CurrentPlan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem powerManagementToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private NotifyIcon notifyIcon1;
+        private ContextMenuStrip contextMenuNotifyIcon;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripSeparator toolStripMenuItem1;
+        private ToolStripSeparator toolStripMenuItem3;
+        private ToolStripMenuItem quitToolStripMenuItem1;
+        private ToolStripSeparator quitToolStripMenuItem;
+        private ToolStripMenuItem quitToolStripMenuItem2;
+        private Button Hide;
+        private CheckBox startSystemTrayCheckbox;
+    }
+}
+
---
+++ PowerManagement/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PowerManagement
+{
+    public partial class MainWindow : Form
+    {
+        [DllImport("PowrProf.dll")]
+        public static extern UInt32 PowerEnumerate(IntPtr RootPowerKey, IntPtr SchemeGuid, IntPtr SubGroupOfPowerSettingGuid, UInt32 AcessFlags, UInt32 Index, ref Guid Buffer, ref UInt32 BufferSize);
+
+        [DllImport("PowrProf.dll")]
+        public static extern UInt32 PowerReadFriendlyName(IntPtr RootPowerKey, ref Guid SchemeGuid, IntPtr SubGroupOfPowerSettingGuid, IntPtr PowerSettingGuid, IntPtr Buffer, ref UInt32 BufferSize);
+
+        [DllImportAttribute("powrprof.dll", EntryPoint = "PowerSetActiveScheme")]
+        public static extern uint PowerSetActiveScheme(IntPtr UserPowerKey, ref Guid ActivePolicyGuid);
+
+        [DllImportAttribute("powrprof.dll", EntryPoint = "PowerGetActiveScheme")]
+        public static extern uint PowerGetActiveScheme(IntPtr UserPowerKey, out IntPtr ActivePolicyGuid);
+        
+        int selected = 0;
+        IEnumerable<Guid> powerPlans;
+        ArrayList pow;
+        public MainWindow()
+        {
+            InitializeComponent();
+            powerPlans = GetAll();
+            CurrentPlan.Text = "Current Plan: " + ReadFriendlyName(GetActiveGuid());
+            pow = new ArrayList();
+            foreach (Guid item in powerPlans) {
+                string planString = ReadFriendlyName(item);
+                ToolStripMenuItem newMenuItem = new ToolStripMenuItem();
+                newMenuItem.Name = planString+"ToolStripMenu";
+                newMenuItem.Text = planString;
+                contextMenuNotifyIcon.Items.Insert(2,newMenuItem);
+                comboBoxPower.Items.Add(planString);
+                pow.Add(item);
+            }
+            comboBoxPower.SelectedIndex = 0;
+
+            if (!System.IO.Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\PowerManagement"))
+            {
+                System.IO.Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\PowerManagement");
+            }
+
+            if (!System.IO.File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\PowerManagement\\settings.ini"))
+            {
+                string[] lines = { "StartInSystemTray", "0" };
+                System.IO.File.WriteAllLines(@"" + Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\PowerManagement\\settings.ini", lines);
+            }
+        }
+
+        private void formShown_(object sender, EventArgs e)
+        {
+            String[] lines = System.IO.File.ReadAllLines(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\PowerManagement\\settings.ini");
+            if (lines[1] == "0")
+            {
+                startSystemTrayCheckbox.Checked = false;
+            }
+            else if (lines[1] == "1")
+            {
+                startSystemTrayCheckbox.Checked = true;
+                this.Hide();
+            }
+        }
+
+        public enum AccessFlags : uint
+        {
+            ACCESS_SCHEME = 16,
+            ACCESS_SUBGROUP = 17,
+            ACCESS_INDIVIDUAL_SETTING = 18
+        }
+        /*
+         Obtiene todos los planes de energía
+        */
+        public static IEnumerable<Guid> GetAll()
+        {
+            var schemeGuid = Guid.Empty;
+
+            uint sizeSchemeGuid = (uint)Marshal.SizeOf(typeof(Guid));
+            uint schemeIndex = 0;
+
+            while (PowerEnumerate(IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, (uint)AccessFlags.ACCESS_SCHEME, schemeIndex, ref schemeGuid, ref sizeSchemeGuid) == 0)
+            {
+                yield return schemeGuid;
+                schemeIndex++;
+            }
+        }
+        /*
+        Obtiene el nombre de un plan de energía dado en el equipo. 
+        */
+        private static string ReadFriendlyName(Guid schemeGuid)
+        {
+            uint sizeName = 1024;
+            IntPtr pSizeName = Marshal.AllocHGlobal((int)sizeName);
+
+            string friendlyName;
+
+            try
+            {
+                PowerReadFriendlyName(IntPtr.Zero, ref schemeGuid, IntPtr.Zero, IntPtr.Zero, pSizeName, ref sizeName);
+                friendlyName = Marshal.PtrToStringUni(pSizeName);
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(pSizeName);
+            }
+
+            return friendlyName;
+        }
+
+       
+        /*
+        Devuelve el plan de energía usado actualmente 
+        */
+        private Guid GetActiveGuid()
+        {
+            Guid ActiveScheme = Guid.Empty;
+            IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(IntPtr)));
+            if (PowerGetActiveScheme((IntPtr)null, out ptr) == 0)
+            {
+                ActiveScheme = (Guid)Marshal.PtrToStructure(ptr, typeof(Guid));
+                if (ptr != null)
+                {
+                    Marshal.FreeHGlobal(ptr);
+                }
+            }
+            return ActiveScheme;
+        }
+        
+        /*
+        Acción de botón de salida. 
+        */
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void comboBoxPower_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selected = comboBoxPower.SelectedIndex;
+        }
+
+        /*
+        Acción del botón de aplicar el plan de energía 
+        */
+        private void ApplyButton_Click(object sender, EventArgs e)
+        {
+            int ind = comboBoxPower.SelectedIndex;
+            Guid sel = (Guid) pow[ind];
+            PowerSetActiveScheme(IntPtr.Zero, ref sel);
+            CurrentPlan.Text = "Current Plan: " + ReadFriendlyName(GetActiveGuid());
+            notifyIcon1.BalloonTipText = CurrentPlan.Text;
+            notifyIcon1.ShowBalloonTip(1);
+        }
+
+        /*
+        Cambia el plan de energia desde la bandeja de notificaciones 
+        */
+        private void contextMenuNotifyIcon_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            string name = e.ClickedItem.Name;
+            //Elimina ToolStripMenu del nombre del plan de energia del menu de contexto
+            name = name.Remove(name.Length-13, 13);
+
+            foreach (Guid i in pow)
+            {
+                if (ReadFriendlyName(i) == name)
+                {
+                    Guid sel = (Guid)i;
+                    PowerSetActiveScheme(IntPtr.Zero, ref sel);
+                    CurrentPlan.Text = "Current Plan: " + ReadFriendlyName(GetActiveGuid());
+                }
+            }
+            if (e.ClickedItem.Name != "openToolStripMenuItem" && e.ClickedItem.Name != "quitToolStripMenuItem2")
+            {
+                notifyIcon1.BalloonTipText = CurrentPlan.Text;
+                notifyIcon1.ShowBalloonTip(1);
+            }
+        } 
+
+
+        /*
+        Acción de salir en archivo 
+        */
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        /*
+        Ventana de acerca de... 
+        */
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("PowerManagement 2.0 \nby elssbbboy", "About", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+        }
+
+        private void quitToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }      
+        
+
+        private void Hide_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void startSystemTrayCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            string settingsPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)+"\\PowerManagement";
+            string[] lines = { "StartInSystemTray", "1" };
+            if (startSystemTrayCheckbox.Checked)
+            {
+                System.IO.File.WriteAllLines(@"" + settingsPath + "\\settings.ini", lines);
+            }
+            else
+            {
+                lines[1] = "0";
+                System.IO.File.WriteAllLines(@"" + settingsPath + "\\settings.ini", lines);
+            }
+        }
+    }
+}
